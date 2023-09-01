af343081e31182eccb64a9201395084459267956
Oséias Costa <oseiasc2@gmail.com>
2023-09-01 14:33:35 +0000

    feat(Location): create Location component section

---
+++ components/Location/location.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const LocationContainer = styled.section``;
+
+export const Map = styled.iframe`
+  box-shadow: 0px 4px 35px 0px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+`;
+
+export const LocationTitle = styled.h2`
+  padding-top: 27px;
+  padding-bottom: 37px;
+`;
