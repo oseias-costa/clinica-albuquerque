3c3e46e46566dc2097909594f7260f49a3268b55
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-09-04 10:51:36 -0300

    fix(FooterMidias): fixed target social midias buttons

--- components/Footer/FooterMidias.tsx
+++ components/Footer/FooterMidias.tsx
@@ -5,10 +5,10 @@ import Face from "../../public/face.png";
 export default function FooterMidias(){
     return(
         <FooterMidiasContainer>
-            <a href='https://www.instagram.com/spimplantes/'>
+            <a href='https://www.instagram.com/spimplantes/' target='_blank'>
                 <IconMidia src={Insta} alt='Instagram da Clínica Albuquerque' />
             </a>
-            <a href='https://www.facebook.com/spimplantes/?locale=pt_BR'>
+            <a href='https://www.facebook.com/spimplantes/?locale=pt_BR' target='_blank'>
                 <IconMidia src={Face} alt='Facebook da Clínica Albuquerque' />
             </a>
         </FooterMidiasContainer>
